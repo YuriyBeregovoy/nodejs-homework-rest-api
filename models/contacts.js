9f63b980ad19056091dba9ff6a3f5abf2684249d
YuriyBeregovoy <rik0681@gmail.com>
2023-11-10 09:44:23 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,6 +1,6 @@
 const fs = require('fs/promises')
 const path = require('path');
-const { nanoid } = require('nanoid');
+const newId = uuidv4();
 
 
 const contactsPath = path.resolve("./models/contacts.json");
@@ -31,7 +31,7 @@ const removeContact = async (contactId) => {
 
 const addContact = async (body) => {
    const contacts = await listContacts();
-  const newContact = {id: nanoid(), ...body };
+  const newContact = {id: newId, ...body };
   contacts.push(newContact);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return newContact;
