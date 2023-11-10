a96d3b4f0391c98a513149631c4eb6f44051c1a9
YuriyBeregovoy <rik0681@gmail.com>
2023-11-10 12:17:56 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -12,8 +12,10 @@ const {
 const router = express.Router()
 
 const addSchema = Joi.object({
-  
-})
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().pattern(/^\(\d{3}\) \d{3}-\d{4}$/).required(),
+});
 
 router.get('/', async (req, res, next) => {
   try {
