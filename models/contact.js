d04c2a07c744179a2937614fba959413032fe4e4
YuriyBeregovoy <rik0681@gmail.com>
2023-11-16 21:33:39 +0200

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -1,13 +1,22 @@
 const { Schema, model } = require("mongoose")
-const Joi = require('joi');
  
-const contactSchema = new Schema({
-   name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().pattern(/^\(\d{3}\) \d{3}-\d{4}$/).required(),
-  favorite: Joi.boolean().required(),
-
-});
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  });
 
 const Contact = model("contact", contactSchema);
 
