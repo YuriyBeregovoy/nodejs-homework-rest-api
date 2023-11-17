68c563844eefb837172f134117266257b004ddac
YuriyBeregovoy <rik0681@gmail.com>
2023-11-17 16:35:26 +0200

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+const isValidId = require("../../middlewares/isValidId")
 const {
   getAll,
   getById,
   add,
-  // updateById,
-  // deleteById
+  updateById,
+  deleteById
 } = require("../../controllers/contacts");
 
 const router = express.Router();
@@ -13,12 +13,12 @@ const router = express.Router();
 
 router.get('/', getAll);
 
-router.get('/:contactId', getById )
+router.get('/:contactId', isValidId, getById )
 
 router.post('/', add)
 
-// router.put('/:contactId', updateById)
+router.put('/:contactId', isValidId, updateById)
 
-// router.delete('/:contactId', deleteById)
+router.delete('/:contactId', isValidId, deleteById)
 
 module.exports = router;
