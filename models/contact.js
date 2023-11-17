fdb3ee0218be3297cc9c6ba00e22b25891e47944
YuriyBeregovoy <rik0681@gmail.com>
2023-11-17 22:03:30 +0200

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -22,13 +22,20 @@ const contactSchema = new Schema(
 
 contactSchema.post("save", hahdleMongooseError);
 
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().pattern(/^\(\d{3}\) \d{3}-\d{4}$/).required(),
+  favorite: Joi.boolean()
+});
+
 const updateFavoriteSchema = Joi.object({
   favorite: Joi.boolean().required(),
 })
 
 const Contact = model("contact", contactSchema);
 
-module.exports = {Contact, updateFavoriteSchema};
+module.exports = {Contact, updateFavoriteSchema, addSchema};
 
 
 
