78da67ad0c95a8f527051be85a58d69e2e4045ea
YuriyBeregovoy <rik0681@gmail.com>
2023-11-17 20:36:16 +0200

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -36,6 +36,18 @@ const updateById = async (req, res) => {
   res.json(result);
 };
 
+
+const updateStatusContact = async (req, res) => {
+   const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
+  if (!result) {
+    throw HttpError(404, "missing field favorite");
+  }
+  res.json(result);
+};
+
+
+
 const deleteById = async (req, res) => {
   const { contactId } = req.params;
   const result = await Contact.findByIdAndRemove(contactId);
@@ -51,5 +63,6 @@ module.exports = {
   getById: ctrlWrapper(getById),
   add: ctrlWrapper(add),
   updateById: ctrlWrapper(updateById),
-  deleteById: ctrlWrapper(deleteById)
+  deleteById: ctrlWrapper(deleteById),
+  updateStatusContact: ctrlWrapper(updateStatusContact)
 };
