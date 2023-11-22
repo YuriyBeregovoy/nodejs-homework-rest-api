92ac2fba056897e93b8546468cadd59b3cdf1b34
YuriyBeregovoy <rik0681@gmail.com>
2023-11-22 15:59:13 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -44,15 +44,13 @@ const loginSchema = Joi.object({
   password: Joi.string().min(6).required(),
 });
 
-const schemas = {
-  registerSchema,
-  loginSchema,
-};
+
 
 
 const User = model("user", userSchema);
 
 module.exports = {
   User,
-  schemas,
+  registerSchema,
+  loginSchema,
 }
