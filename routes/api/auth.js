4b9ef3ca48d846a16f3325f1615ffee1b234d388
YuriyBeregovoy <rik0681@gmail.com>
2023-11-22 14:39:58 +0200

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {schemas} = require("../../models/user")
+const { schemas } = require("../../models/user");
 
+const ctrl = require("../../controllers/authControll");
 
-router.post("/register", schemas.registerSchema);
+router.post("/register", schemas.registerSchema, ctrl.register);
 
 module.exports = router; 
