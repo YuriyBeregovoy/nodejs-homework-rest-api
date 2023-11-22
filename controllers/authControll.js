5d17db16e208f5111458667f027b799efa42b531
YuriyBeregovoy <rik0681@gmail.com>
2023-11-22 14:37:10 +0200

    Create authControll.js

---
+++ controllers/authControll.js
@@ -0,0 +1,18 @@
+const { User } = require("../models/user");
+// const HttpError = require('http-errors');
+const ctrlWrapper = require('../helpers/ctrlWrapper');
+
+
+const register = async (req, res) => {
+  const newUser = await User.create(req.body);
+
+  res.json({
+    email: newUser.email,
+  })
+
+};
+
+module.exports = {
+  register: ctrlWrapper(register),
+
+}
