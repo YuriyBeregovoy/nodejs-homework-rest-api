04f581d2c7aba951ed0a6de0add860b0d830758d
YuriyBeregovoy <rik0681@gmail.com>
2023-11-22 23:06:28 +0200

    Update authControll.js

--- controllers/authControll.js
+++ controllers/authControll.js
@@ -1,17 +1,20 @@
 const { User } = require("../models/user");
 const HttpError = require('http-errors');
 const ctrlWrapper = require('../helpers/ctrlWrapper');
-
+const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
 
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
     throw HttpError(409, "Email already sn use");
   }
-  const newUser = await User.create(req.body);
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({...req.body, password: hashPassword});
 
  res.status(201).json(newUser);
 
