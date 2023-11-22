415d6f7a1aebb733504408f3a50b2132ade86732
YuriyBeregovoy <rik0681@gmail.com>
2023-11-22 15:43:42 +0200

    Update handleMongooseError.js

--- helpers/handleMongooseError.js
+++ helpers/handleMongooseError.js
@@ -1,6 +1,6 @@
-const hahdleMongooseError = (error, data, next) => {
+const handleMongooseError = (error, data, next) => {
   error.status = 400;
   next();
 };
 
-module.exports = hahdleMongooseError;
+module.exports = handleMongooseError;
