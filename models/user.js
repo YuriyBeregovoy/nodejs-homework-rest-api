a6efea37b24f1dd9cde0335cde58a5e74fcae2b3
YuriyBeregovoy <rik0681@gmail.com>
2023-11-22 11:28:44 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,23 +2,31 @@ const { Schema, model } = require("mongoose");
 const hahdleMongooseError = require("../helpers/handleMongooseError");
 const Joi = require("joi");
 
-const userSchema = new Schema(
-{
-  password: {
-    type: String,
-    required: [true, 'Set password for user'],
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    unique: true,
-  },
-  subscription: {
-    type: String,
-    enum: ["starter", "pro", "business"],
-    default: "starter"
-  },
-  token: String
-}
 
-)
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, 'Set password for user'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      match: emailRegexp,
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter"
+    },
+    token: String
+  }
+
+);
+
+userSchema.post("save", hahdleMongooseError);
+
