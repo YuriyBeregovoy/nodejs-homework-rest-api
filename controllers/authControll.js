d9a166c6d5779d42f643ad64e2393ab799a41e2f
YuriyBeregovoy <rik0681@gmail.com>
2023-11-22 15:43:40 +0200

    Update authControll.js

--- controllers/authControll.js
+++ controllers/authControll.js
@@ -1,9 +1,13 @@
-const { User } = require("../models/user");
-// const HttpError = require('http-errors');
+const { User, registerSchema } = require("../models/user");
+const HttpError = require('http-errors');
 const ctrlWrapper = require('../helpers/ctrlWrapper');
 
 
 const register = async (req, res) => {
+  const { error } = registerSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, error.message);
+  }
   const newUser = await User.create(req.body);
 
   res.json(newUser);
