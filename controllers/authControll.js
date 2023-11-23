b52c38b6705bd23b21b34516a90f8ba262689ad9
YuriyBeregovoy <rik0681@gmail.com>
2023-11-23 09:58:15 +0200

    Update authControll.js

--- controllers/authControll.js
+++ controllers/authControll.js
@@ -16,7 +16,11 @@ const register = async (req, res) => {
 
   const newUser = await User.create({...req.body, password: hashPassword});
 
- res.status(201).json(newUser);
+  res.status(201).json({
+    user: {
+      email: newUser.email,
+      subscription: newUser.subscription,
+ }});
 
 };
 
