aaf0ecaf0731cbf2c7ae7bfa3e87298e66d70f71
YuriyBeregovoy <rik0681@gmail.com>
2023-11-23 19:52:18 +0200

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,5 +1,22 @@
+const HttpError = require('http-errors');
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = process.env;
+
+
 const authenticate = async (req, res, next) => {
-  
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+  if (bearer !== "Bearer") {
+    next(HttpError(401))
+  }
+  try {
+    const { id } = jwt.verify(token, SECRET_KEY);
+
+  }
+  catch {
+        next(HttpError(401))
+
+  }
 };
 
 module.exports = authenticate;
