a9964b5dafb5e105db822b37b507cf615a67040d
YuriyBeregovoy <rik0681@gmail.com>
2023-11-23 22:23:39 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -23,10 +23,7 @@ const userSchema = new Schema(
       enum: ["starter", "pro", "business"],
       default: "starter"
     },
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: 'user',
-    },
+   
     token: String
   }
 
