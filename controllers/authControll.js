c80a5f4ff7a13b544b9909251ae4570215664db4
YuriyBeregovoy <rik0681@gmail.com>
2023-11-23 11:44:32 +0200

    Update authControll.js

--- controllers/authControll.js
+++ controllers/authControll.js
@@ -6,21 +6,17 @@ const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = process.env;
 
 
-
 const register = async (req, res) => {
-  
     const { error } = registerSchema.validate(req.body);
   if (error) {
     throw HttpError(400, "Bad Request");
   }
-
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
     throw HttpError(409, "Email already sn use");
   }
-
   const hashPassword = await bcrypt.hash(password, 10);
   const newUser = await User.create({...req.body, password: hashPassword});
 
@@ -32,40 +28,32 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-
   const { email, password } = req.body;
-
-  const { error } =loginSchema.validate(req.body);
+  const { error } = loginSchema.validate(req.body);
   if (error) {
     throw HttpError(400, "Bad Request");
   }
   const user = await User.findOne({ email });
   if (!user) {
-        throw HttpError(401, "Email or password is wrong");
+    throw HttpError(401, "Email or password is wrong");
   }
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
-            throw HttpError(401, "Email or password is wrong");
+    throw HttpError(401, "Email or password is wrong");
   }
-
   const payload = {
     id: user._id,
   };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
 
-  const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
-
- res.status(200).json({
-   token,
+  res.status(200).json({
+    token,
     user: {
       email: user.email,
       subscription: user.subscription,
- }
-  })
-
-  
-}
-
-
+    }
+  });
+};
 
 
 
