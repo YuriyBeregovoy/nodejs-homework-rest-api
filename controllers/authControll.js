e7c6041b28e766da916bc0253541753574237fb3
YuriyBeregovoy <rik0681@gmail.com>
2023-11-23 21:07:36 +0200

    Update authControll.js

--- controllers/authControll.js
+++ controllers/authControll.js
@@ -44,7 +44,7 @@ const login = async (req, res) => {
   const payload = {
     id: user._id,
   };
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "0h" });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
 
   res.status(200).json({
     token,
