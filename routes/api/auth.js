0e10579b8c344145787fcbc029e115e47aa1887c
YuriyBeregovoy <rik0681@gmail.com>
2023-11-23 10:38:45 +0200

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 // const isValidId = require("../../middlewares/isValidId")
 
-const { register} = require("../../controllers/authControll");
+const { register, login} = require("../../controllers/authControll");
 
 router.post("/users/register", register);
+
+router.post("/users/login", login);
+
 
 module.exports = router; 
 
