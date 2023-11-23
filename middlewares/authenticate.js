0ecca15483113476cc01efb51d750c77ef25a0ce
YuriyBeregovoy <rik0681@gmail.com>
2023-11-23 21:25:52 +0200

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,19 +9,19 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer") {
-    next(HttpError(401));
+    next(HttpError(401, "Not authorized"));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (!user) {
-      next(HttpError(401));
+      next(HttpError(401, "Not authorized"));
     }
     next();
 
   }
   catch {
-    next(HttpError(401));
+    next(HttpError(401, "Not authorized"));
 
   }
 };
