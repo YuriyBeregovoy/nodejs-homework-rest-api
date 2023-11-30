4e3a58f9abe8383f438f02263e299c0ca9efd883
YuriyBeregovoy <rik0681@gmail.com>
2023-11-30 13:18:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use("/users", authRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+  res.status(404).json({ message: 'Not found' }) 
 })
 
 app.use((err, req, res, next) => {
