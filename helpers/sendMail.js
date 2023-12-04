c19c4d7c0c4923688ec11005b0e886c925a1f318
YuriyBeregovoy <rik0681@gmail.com>
2023-12-04 10:44:45 +0200

    Update sendMail.js

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,28 +1,55 @@
-const nodemailer = require("nodemailer");
-require("dotenv").config();
+// const nodemailer = require("nodemailer");
+// require("dotenv").config();
 
-const { META_PASSWORD } = process.env;
+// const { META_PASSWORD } = process.env;
 
-const nodemailerCnfg = {
-  host: "smtp.meta.ua",
-  port: 465,
-  secure: true,
+// const nodemailerCnfg = {
+//   host: "smtp.meta.ua",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: 'yuriy_beregovoy@meta.ua',
+//     pass: META_PASSWORD
+//   }
+
+// };
+
+// const transport = nodemailer.createTransport(nodemailerCnfg);
+
+// const email = {
+//   to: 'recipient@example.com',
+//   from: "yuriy_beregovoy@meta.ua",
+//   subject: "Test email",
+//   html: "<p><strong>Test email</strong> from localhost: 3000</p>"
+// };
+
+// transport.sendMail(email)
+//   .then(() => console.log("Email send success"))
+//   .catch(error => console.log(error.message));
+
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: 'sandbox.smtp.mailtrap.io',
+  port: 2525,
   auth: {
-    user: 'yuriy_beregovoy@meta.ua',
-    pass: META_PASSWORD
+    user: 'b3f4363e988e65',
+    pass: '********af0a'
   }
+});
 
+const mailOptions = {
+  from: 'from@example.com',
+  to: 'to@example.com',
+  subject: 'Test Email',
+  text: 'This is a test email from Mailtrap.'
 };
 
-const transport = nodemailer.createTransport(nodemailerCnfg);
-
-const email = {
-  to: 'recipient@example.com',
-  from: "yuriy_beregovoy@meta.ua",
-  subject: "Test email",
-  html: "<p><strong>Test email</strong> from localhost: 3000</p>"
-};
-
-transport.sendMail(email)
-  .then(() => console.log("Email send success"))
-  .catch(error => console.log(error.message));
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
