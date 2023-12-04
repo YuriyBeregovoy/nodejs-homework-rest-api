58a48e4d39219d182aaf9528a234f8011dd61e28
YuriyBeregovoy <rik0681@gmail.com>
2023-12-04 10:17:02 +0200

    Update sendMail.js

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,6 +1,8 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
+
+const sendMail = async (emailOptions) => {
 const { META_PASSWORD } = process.env;
 
 const nodemailerCnfg = {
@@ -16,13 +18,25 @@ const nodemailerCnfg = {
 
 const transport = nodemailer.createTransport(nodemailerCnfg);
 
-const email = {
-  to: "yuriy_beregovoy@meta.ua",
-  from: "yuriy_beregovoy@meta.ua",
-  subject: "Test email",
-  html: "<p><strong>Test email</strong> from localhost: 3000</p>"
+// const email = {
+//   to: "yuriy_beregovoy@meta.ua",
+//   from: "yuriy_beregovoy@meta.ua",
+//   subject: "Test email",
+//   html: "<p><strong>Test email</strong> from localhost: 3000</p>"
+// };
+
+// transport.sendMail(email)
+//   .then(() => console.log("Email send success"))
+//   .catch(error => console.log(error.message));
+
+  try {
+    const info = await transport.sendMail(emailOptions);
+    console.log("Email sent: " + info.response);
+    return info;
+  } catch (error) {
+    console.error("Error sending email: ", error.message);
+    throw new Error("Error sending email");
+  }
 };
 
-transport.sendMail(email)
-  .then(() => console.log("Email send success"))
-  .catch(error => console.log(error.message));
+module.exports = sendMail;
